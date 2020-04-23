606e5a128dece89cb9491df270c578229af3c1f1
Mac <mac@wumao.local>
2020-04-23 22:43:08 +0800

    dev

--- vue3.0/src/views/login.vue
+++ vue3.0/src/views/login.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="wrap">
-   {{msg}}
-  </div>
+  <div class="wrap">{{msg}}</div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
   components: {},
   data() {
-	  return {
-		  msg:"这里是登录页"
-	  }
-  },
-}
+    return {
+      msg: "这里是登录页"
+    };
+  }
+};
 </script>
 <style lang="scss" scoped>
-	.wrap{
-		height: 100vh;
-		background: green;
-	}
+.wrap {
+  height: 100vh;
+  background: green;
+}
 </style>

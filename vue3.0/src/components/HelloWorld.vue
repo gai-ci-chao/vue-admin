cf3b4d6f8a3eda4322011d746848a331728de086
Mac <mac@wumao.local>
2020-04-26 21:26:05 +0800

    dev

--- vue3.0/src/components/HelloWorld.vue
+++ vue3.0/src/components/HelloWorld.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+	<div></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   props: {
     msg: String
-  }
-}
+  },
+  data() {
+    return {
+      sdfklasd: {
+        min: 12,
+        max: 100,
+        id: 29,
+        dis: "nishishui"
+      }
+    };
+  },
+  created() {
+	  
+  },
+  methods: {
+	//   sldk(){
+	// 	  this.$emit("adlsl",this.dicsl)
+	//   }
+  },
+  
+};
 </script>
 
 <style scoped lang="scss">
--- vue3.0/src/views/login.vue
+++ vue3.0/src/views/login.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="wrap">{{msg}}</div>
+  <div class="wrap">
+	<button @click="sldkfjls">获取数据按钮</button>
+	<hello-world ref="slkdfj" > </hello-world>
+  </div>
 </template>
 
 <script>
+import HelloWorld from "../components/HelloWorld"
 export default {
   name: "Home",
-  components: {},
+  components: {
+	  HelloWorld
+  },
   data() {
     return {
       msg: "这里是登录页"
     };
-  }
+  },
+  methods: {
+	  sldkfjls(){
+		  console.log(this.$refs.slkdfj);
+		  console.log(this.$refs.slkdfj.sdfklasd);
+	  }
+  },
 };
 </script>
 <style lang="scss" scoped>
